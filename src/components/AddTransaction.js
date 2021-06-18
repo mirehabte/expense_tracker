51955b53ead36cfe8093d429b59b15ab467025cd
Mehreteab <mirehabtecoen@gmail.com>
2021-06-18 12:49:21 -0700

    add and delete transaction added

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 const AddTransaction = () => {
+
+    const { addTransaction } = useContext(GlobalContext)
 
     const [text, setText] = useState('')
     const [amount, setAmount] = useState('')
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount //change amount to number
+        }
+
+        addTransaction(newTransaction)
+
+        setText('')
+        setAmount('')
+    }
+
     return (
         <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={ onSubmit }>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" placeholder="Enter text ..." 
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const Transaction = ({ transaction }) => {
+
+    const { deleteTransaction } = useContext(GlobalContext)
 
     const sign = transaction.amount < 0 ? '-' : '+';
     const signColor = transaction.amount < 0 ? 'minus' : 'plus'
 
     return (
         <li className={signColor}>
-            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
     )
 }
